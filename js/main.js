7190335ea8f520199feaf343270363b3507141f2
saichi <s.wakeup31@gmail.com>
2014-08-11 03:15:22 +0900

    Added Battery Manager

--- js/main.js
+++ js/main.js
@@ -1,7 +1,55 @@
 window.onload = function () {
-  alert("hello");
-  $("#fbutton").click(function () {
-        $("#fdiv").text("JSは最高だぜ");
+  $("#tab1").click(function () {
+    updateBatteryStatus();
   });
+
+  $("#updateButton").click(function () {
+    updateBatteryStatus();
+
+    var power = navigator.mozPower || navigator.power || navigator.webkitPower;
+    if (power.screenEnabled) {
+      power.screenBrightness = 0.5;
+    }
+  });
+
 };
+
+  //sessionStorage.setItem("username", "John");
+  //alert( "username = " + sessionStorage.getItem("username"));
+
+  var battery = navigator.battery || navigator.mozBattery || navigator.webkitBattery;
+  function updateBatteryStatus(){
+    $('#container').highcharts({
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false
+        },
+        title: {
+            text: 'Battery Status'
+        },
+        tooltip: {
+          pointFormat: '{series.name}: <b>{point.percentage:.0f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: false
+                },
+                showInLegend: true
+            }
+        },
+        series: [{
+            type: 'pie',
+            name: 'Browser share',
+            data: [
+                ['BatteryCharge',   battery.level * 100],
+                ['UsedBattery',       100 - battery.level * 100]
+            ]
+        }]
+    });
+  }
+
 
