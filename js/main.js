e3f6b4e111e2c2fc9127104a370cdfed4d2ba87b
saichi <s.wakeup31@gmail.com>
2014-08-11 04:53:19 +0900

    Added Time parameter

--- js/main.js
+++ js/main.js
@@ -1,55 +1,99 @@
-window.onload = function () {
-  $("#tab1").click(function () {
-    updateBatteryStatus();
+var battery = navigator.battery || navigator.mozBattery || navigator.webkitBattery;
+var selecting_tab = "1"
+function updateBatteryStatus(){
+  $('#container').highcharts({
+      chart: {
+          plotBackgroundColor: null,
+          plotBorderWidth: null,
+          plotShadow: false
+      },
+      title: {
+          text: 'Battery Status'
+      },
+      tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.0f}%</b>'
+      },
+      plotOptions: {
+          pie: {
+              allowPointSelect: true,
+              cursor: 'pointer',
+              dataLabels: {
+                  enabled: false
+              },
+              showInLegend: true
+          }
+      },
+      series: [{
+          type: 'pie',
+          name: 'Percentage',
+          data: [
+              ['BatteryCharge', battery.level * 100],
+              ['UsedBattery', 100 - battery.level * 100]
+          ]
+      }]
   });
+}
 
-  $("#updateButton").click(function () {
-    updateBatteryStatus();
+function updateBatteryLife(){
+  if(battery.charging == true){
+    $('#booleans').text("充電中");
+    $('#battery_type').text("充電完了までの時間");
 
-    var power = navigator.mozPower || navigator.power || navigator.webkitPower;
-    if (power.screenEnabled) {
-      power.screenBrightness = 0.5;
+    var at_time = battery.chargingTime;
+    if(isNaN(parseInt(at_time))){
+      $('#times').text("計測中");
+    }else{
+      $('#times').text(parseInt(at_time/3600) + "時間" + (at_time%3600/60) + "分");
     }
-  });
 
-};
+  }else{
+    $('#booleans').text("放電中");
+    $('#battery_type').text("充電切れまでの時間");
 
-  //sessionStorage.setItem("username", "John");
-  //alert( "username = " + sessionStorage.getItem("username"));
+    var at_time = battery.dischargingTime;
+    if(isNaN(parseInt(at_time))){
+      $('#times').text("計測中");
+    }else{
+      $('#times').text(parseInt(at_time/3600) + "時間" + (at_time%3600/60) + "分");
+    }
 
-  var battery = navigator.battery || navigator.mozBattery || navigator.webkitBattery;
-  function updateBatteryStatus(){
-    $('#container').highcharts({
-        chart: {
-            plotBackgroundColor: null,
-            plotBorderWidth: null,
-            plotShadow: false
-        },
-        title: {
-            text: 'Battery Status'
-        },
-        tooltip: {
-          pointFormat: '{series.name}: <b>{point.percentage:.0f}%</b>'
-        },
-        plotOptions: {
-            pie: {
-                allowPointSelect: true,
-                cursor: 'pointer',
-                dataLabels: {
-                    enabled: false
-                },
-                showInLegend: true
-            }
-        },
-        series: [{
-            type: 'pie',
-            name: 'Browser share',
-            data: [
-                ['BatteryCharge',   battery.level * 100],
-                ['UsedBattery',       100 - battery.level * 100]
-            ]
-        }]
-    });
   }
+}
+
+battery.onlevelchange = function() {
+  if(selecting_tab == "1"){
+    updateBatteryStatus();  
+  }
+}
+
+battery.onchargingtimechange = function(){
+  if(selecting_tab == "2"){
+    updateBatteryLife();
+  }
+}
+
+battery.ondischargingtimechange = function(){
+  if(selecting_tab == "2"){
+    updateBatteryLife();
+  }
+}
+
+battery.onchargingchange = function(){
+  if(selecting_tab == "2"){
+    updateBatteryLife();
+  }
+}
+
+window.onload = function () {
+  updateBatteryStatus();
+  $("#tab1").click(function () {
+    selecting_tab = "1"
+    updateBatteryStatus();
+  });
+  $("#tab2").click(function () {
+    selecting_tab = "2"
+    updateBatteryLife();
+  });
+};
 
 
